407d283083b481ff74c8fae343ce2f6c6702911f
Rafiqul Shopon <rafiqul.dev@gmail.com>
2024-02-20 15:50:28 +0600

    Add calculator function and test cases for arithmetic operations

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,15 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  if (operator === '+') {
+    return num1 + num2;
+  } else if (operator === '-') {
+    return num1 - num2;
+  } else if (operator === '*') {
+    return num1 * num2;
+  } else if (operator === '/') {
+    return num1 / num2;
+  } else {
+    return 'Invalid operator';
+  }
+}
 
 module.exports = calculator;
