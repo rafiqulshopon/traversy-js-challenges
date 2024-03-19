6c1c619c10a928d2e4eb9908f60cfacc5a7f6f93
Rafiqul Shopon <rafiqul.dev@gmail.com>
2024-03-19 17:28:31 +0600

    solve palindrome problem

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,13 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  const cleanedStr = str.toLowerCase().replace(/[\W_]/g, '');
+  const len = cleanedStr.length;
+
+  for (let i = 0; i < len / 2; i++) {
+    if (cleanedStr[i] !== cleanedStr[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 module.exports = isPalindrome;
