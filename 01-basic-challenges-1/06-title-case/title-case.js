3965493e176ce15924069f6d45e88677940c0f0c
Rafiqul Shopon <rafiqul.dev@gmail.com>
2024-03-19 16:19:21 +0600

    solve title case problem

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,11 @@
-function titleCase() {}
+function titleCase(str) {
+  let words = str.toLowerCase().split(' ');
+
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+  }
+
+  return words.join(' ');
+}
 
 module.exports = titleCase;
