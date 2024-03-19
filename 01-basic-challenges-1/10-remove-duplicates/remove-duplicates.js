da5aba4a3ca1d66a7acace096a399d45986a56e7
Rafiqul Shopon <rafiqul.dev@gmail.com>
2024-03-19 17:43:57 +0600

    solve remove duplicates problem

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,13 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  const finalOutput = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!finalOutput.includes(arr[i])) {
+      finalOutput.push(arr[i]);
+    }
+  }
+
+  return finalOutput;
+}
 
 module.exports = removeDuplicates;
