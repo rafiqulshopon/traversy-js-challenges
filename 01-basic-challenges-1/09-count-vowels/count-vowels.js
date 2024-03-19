f232a16c559d04fb8446f04d83bc9be8dc8ecbde
Rafiqul Shopon <rafiqul.dev@gmail.com>
2024-03-19 17:38:03 +0600

    solved vowel count problem

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,16 @@
-function countVowels() {}
+function countVowels(str) {
+  const formattedString = str.toLowerCase();
+  let count = 0;
+
+  const vowels = 'aeiou';
+
+  for (let char of formattedString) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+
+  return count;
+}
 
 module.exports = countVowels;
